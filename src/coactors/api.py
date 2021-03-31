48a6330512d170907d93c686b0cea430855d6422
Alisa Modeste <softwarepuzzler@gmail.com>
2021-03-30 23:09:56 -0400

    More on the navbar;Cleanup;Updated API to work with GAE -- can't test yet

--- src/coactors/api.py
+++ src/coactors/api.py
@@ -9,16 +9,42 @@ class API:
     # url = "https://api.themoviedb.org/3/movie/550?api_key="
         url = "https://api.themoviedb.org/3" + route
         
-        apiKey = os.environ['THEMOVIEDB_APIKEY'] 
-        querystring['api_key']= apiKey
+        api_key = API.get_api_key()
+        
+        querystring['api_key']= api_key
 
-        # headers = {
-        #   'x-rapidapi-key': apiKey,
-        #   'x-rapidapi-host': "imdb8.p.rapidapi.com"
-        #   }
-        # app.api_count += 1
-        # response = requests.request("GET", url, headers=headers, params=querystring)
         response = requests.request("GET", url, params=querystring)
-
-        # print(response.text)    
+    
         return response.text
+
+    @staticmethod
+    def access_secret_version(project_id, secret_id, version_id):
+        """
+        Access the payload for the given secret version if one exists. The version
+        can be a version number as a string (e.g. "5") or an alias (e.g. "latest").
+        """
+
+        # Import the Secret Manager client library.
+        from google.cloud import secretmanager
+
+        # Create the Secret Manager client.
+        client = secretmanager.SecretManagerServiceClient()
+
+        # Build the resource name of the secret version.
+        name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
+
+        # Access the secret version.
+        response = client.access_secret_version(request={"name": name})
+
+        return response.payload.data.decode("UTF-8")
+
+    @staticmethod
+    def get_api_key():
+        try:
+            return os.environ['THEMOVIEDB_APIKEY']
+        except KeyError:
+            print("getting the secret - print")
+
+            api_key = API.access_secret_version("508429297891", "THEMOVIEDB_APIKEY", 1)
+            os.environ['THEMOVIEDB_APIKEY'] = api_key
+            return api_key
