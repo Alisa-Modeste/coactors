a698d95b901f98fb4aa52b143cd99abe7d6e14a5
Alisa Modeste <softwarepuzzler@gmail.com>
2021-04-03 10:21:58 -0400

    Gunicorn given ample time to complete

--- src/coactors/db_admin.py
+++ src/coactors/db_admin.py
@@ -5,7 +5,7 @@ except ImportError:
     from coactors.api import API
 
 try:
-    neo4j_pw = API.get_secret("NEO4J_PASSWORD")
-    graph = Graph(f"bolt://neo4j:{neo4j_pw}@18.206.151.194:7687")
+    neo4j_pw = API.get_secret("NEO4J_PASSWORD",2)
+    graph = Graph(f"bolt://neo4j:{neo4j_pw}@54.146.245.71:7687")
 except:
     graph = Graph("bolt://neo4j:12345@localhost:7687")
